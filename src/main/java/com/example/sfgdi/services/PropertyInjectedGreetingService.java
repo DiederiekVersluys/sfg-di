4609ae7632d9d00968b1cf7e5cf75bffe97cac10
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-10-11 12:02:33 +0200

    Making beans of services in a config

--- src/main/java/com/example/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/com/example/sfgdi/services/PropertyInjectedGreetingService.java
@@ -1,8 +1,8 @@
 package com.example.sfgdi.services;
 
-import org.springframework.stereotype.Service;
 
-@Service
+
+
 public class PropertyInjectedGreetingService implements GreetingService{
 
     @Override
