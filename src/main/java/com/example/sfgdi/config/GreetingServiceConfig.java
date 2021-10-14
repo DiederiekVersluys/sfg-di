658d774dfb20692ebd6b453f009d12095e52ab9b
DiederiekVersluys <80745300+DiederiekVersluys@users.noreply.github.com>
2021-10-14 15:24:36 +0200

    Petservice java implementation

--- src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/com/example/sfgdi/config/GreetingServiceConfig.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
 
 
-@PropertySource("classpath:datasource.properties")
+
 
 @Configuration
 public class GreetingServiceConfig {
